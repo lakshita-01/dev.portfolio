1ca1892e03f67c04d8e188bcb01e6dcaa995294d
lakshita-01 <lakshitagupta9@gmail.com>
2026-02-11 00:13:12 +0530

    Add Vercel config

--- server/index.js
+++ server/index.js
@@ -74,7 +74,8 @@ const corsOptions = {
       'http://localhost:3000',
       'http://localhost:5173',
       'http://127.0.0.1:3000',
-      'http://127.0.0.1:5173'
+      'http://127.0.0.1:5173',
+      'https://dev-portfolio-mocha-beta.vercel.app/'
     ];
 
     // Allow requests with no origin (mobile apps, etc.)
